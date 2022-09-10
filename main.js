6aa98248c4ab65b7a4fac8c7024afc85f75ef9e5
MAHIRROCHDI <rochdi.mahir@etu.uae.ac.ma>
2022-09-10 19:41:09 +0200

    adding some features using JavaScript

--- main.js
+++ main.js
@@ -9,3 +9,30 @@ span.onclick = function() {
     }
     );
 };
+let nums = document.querySelectorAll(".stats .num") ;
+let statsSection = document.querySelector(".stats") ;
+let spansProg = document.querySelectorAll(".progress span")
+let skillSection = document.querySelector(".skills") ;
+let started = false ;  
+window.onscroll = function() {
+    if( window.scrollY >= statsSection.offsetTop - 100 ){
+        if(!started){
+            nums.forEach((num) => startCount(num)) ; 
+        }
+        started = true ;
+    } 
+    if( window.scrollY >= skillSection.offsetTop ){
+        spansProg.forEach((span) => {
+            span.style.width = span.dataset.width ; 
+        })
+    }
+};
+function startCount(el) {
+    let goal = el.dataset.goal ; 
+    let count = setInterval(() => {
+        el.textContent++ ; 
+        if(el.textContent == goal ){
+            clearInterval(count) ; 
+        }
+    }, 2000 / goal);
+}
