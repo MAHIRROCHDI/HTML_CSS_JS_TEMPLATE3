f8043a3eb5c327e6dc927f59772c52c577a55e35
MAHIRROCHDI <rochdi.mahir@etu.uae.ac.ma>
2022-09-11 16:47:27 +0200

    adding the count down date

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//to top scroll :
 let span = document.querySelector(".up") ;
 window.onscroll = function() {
     this.scrollY >= 1000 ? span.classList.add("show") : span.classList.remove("show") ;
@@ -9,10 +10,9 @@ span.onclick = function() {
     }
     );
 };
+//Increase numbers on scrolling :
 let nums = document.querySelectorAll(".stats .num") ;
 let statsSection = document.querySelector(".stats") ;
-let spansProg = document.querySelectorAll(".progress span")
-let skillSection = document.querySelector(".skills") ;
 let started = false ;  
 window.onscroll = function() {
     if( window.scrollY >= statsSection.offsetTop - 100 ){
@@ -21,11 +21,6 @@ window.onscroll = function() {
         }
         started = true ;
     } 
-    if( window.scrollY >= skillSection.offsetTop ){
-        spansProg.forEach((span) => {
-            span.style.width = span.dataset.width ; 
-        })
-    }
 };
 function startCount(el) {
     let goal = el.dataset.goal ; 
@@ -35,4 +30,40 @@ function startCount(el) {
             clearInterval(count) ; 
         }
     }, 2000 / goal);
-}
+} ; 
+//Animite width on scrolling :
+let spansProg = document.querySelectorAll(".progress span")
+let skillSection = document.querySelector(".our-skills") ;
+window.onscroll = function() {
+    if( window.scrollY >= skillSection.offsetTop ){
+        spansProg.forEach((span) => {
+            span.style.width = span.dataset.width ; 
+        });
+    }
+};
+
+//Create CountDown timer : 
+let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime() ; 
+
+let counter = setInterval(() => {
+    
+    //Get that moment date :
+    let dateNow = new Date().getTime() ;
+
+    //get the Date difference between now and the CountDown Date : 
+    let diffDate = countDownDate - dateNow ;
+
+    //get Time Unit : 
+    let days = Math.floor( diffDate / ( 1000 * 60 * 60 * 24) );
+    let hours = Math.floor((diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) ;
+    let minutes = Math.floor((diffDate % (1000 * 60 * 60 )) / (1000 * 60) ) ;
+    let seconds = Math.floor((diffDate % (1000 * 60)) / 1000 ) ;
+    
+    document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days ; 
+    document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours ;
+    document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes ;
+    document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds ; 
+    if(diffDate < 0) {
+        clearInterval(counter) ; 
+    }
+},1000); 
